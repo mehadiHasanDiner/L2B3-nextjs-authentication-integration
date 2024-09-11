4fac8ab47147db95cd23f37f9e2e83240fd1b3b5
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-09-11 07:09:13 +0600

    first commit

---
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,31 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  return (
+    <div>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Logged-in User email: {session?.user?.email}
+          </h1>
+          <Image
+            src={session?.user?.image}
+            width={100}
+            height={100}
+            alt="user image"
+            className="rounded-full mx-auto"
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DashboardPage;
